a7b8c6a996b90fc0e49fb780b1615946cc8bbb52
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-12-09 20:16:39 +0300

    update apiuri

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
-  apiUrl2: "http://localhost:5500",
-  apiUrl: "https://da-admin-portal-five.vercel.app",
+  apiUrl1: "http://localhost:5500",
+  apiUrl: "http://167.235.17.99:5500",
 };
 
 export default config;
